f5b68bb44c0b9d4678cd95bfd6510ddc7136e28a
BurcaStefan <burcaiulianstefan@gmail.com>
2024-11-03 19:07:42 +0200

    Api CreateContact

---
+++ Application/Use Cases/Commands/ContactC/CreateContactCommand.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Use_Cases.Commands.ContactC
+{
+    public class CreateContactCommand : IRequest<Result<Guid>>
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
