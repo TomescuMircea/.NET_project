b696050cde4ae9f9097427a6ca8515ff4762df77
Stefan Cotos <stefancotos8@gmail.com>
2024-11-03 18:30:09 +0200

    Validator pentru Create API  de la User,Estate,BusinessSpace si Apartment

---
+++ Application/Use Cases/Commands/UserC/CreateUserCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.Use_Cases.Commands.UserC
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.Type).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.FirstName).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.LastName).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.Status).NotEmpty().MaximumLength(100);
+        }
+    }
+}
