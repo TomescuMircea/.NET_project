78a1e90b7ba41f9595b641ac0813d1afa89df394
AlinaBurca <burcaalina348@gmail.com>
2024-11-29 15:43:39 +0200

    modificari

--- SmartRealEstateManagementSystem.IntegrationTests/ContactControllerIntegrationTests.cs
+++ SmartRealEstateManagementSystem.IntegrationTests/ContactControllerIntegrationTests.cs
@@ -1,4 +1,134 @@
-﻿using Application.Use_Cases.Commands.ContactC;
+﻿//using Application.Use_Cases.Commands.ContactC;
+//using Domain.Entities;
+//using FluentAssertions;
+//using Infrastructure.Persistence;
+//using Microsoft.AspNetCore.Mvc.Testing;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+//using System.Net.Http.Json;
+
+//namespace SmartRealEstateManagementSystem.IntegrationTests
+//{
+//    public class ContactControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>, IDisposable
+//    {
+//        private readonly WebApplicationFactory<Program> factory;
+//        private readonly ApplicationDbContext dbContext;
+
+//        private string BaseUrl = "/api/contacts";
+
+//        public ContactControllerIntegrationTests(WebApplicationFactory<Program> factory)
+//        {
+//            // Configure factory to use InMemory database
+//            this.factory = factory.WithWebHostBuilder(builder =>
+//            {
+//                builder.ConfigureServices(services =>
+//                {
+//                    // Remove existing DbContextOptions<ApplicationDbContext>
+//                    var descriptor = services.SingleOrDefault(
+//                        d => d.ServiceType ==
+//                            typeof(DbContextOptions<ApplicationDbContext>));
+
+//                    if (descriptor != null)
+//                    {
+//                        services.Remove(descriptor);
+//                    }
+
+//                    // Add InMemory database for testing
+//                    services.AddDbContext<ApplicationDbContext>(options =>
+//                    {
+//                        options.UseInMemoryDatabase("InMemoryDbForTesting");
+//                    });
+//                });
+//            });
+
+//            // Create service scope to access DbContext
+//            var scope = this.factory.Services.CreateScope();
+//            dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//            dbContext.Database.EnsureCreated();
+//        }
+
+//        [Fact]
+//        public async void GivenContacts_WhenGetAllIsCalled_ThenReturnsTheRightContentType()
+//        {
+//            // arrange
+//            var client = factory.CreateClient();
+
+//            // act
+//            var response = await client.GetAsync(BaseUrl);
+
+//            // assert
+//            response.EnsureSuccessStatusCode();
+//            response.Content.Headers.ContentType?.ToString().Should().Be("application/json; charset=utf-8");
+//        }
+
+//        [Fact]
+//        public async void GivenExistingContacts_WhenGetAllIsCalled_ThenReturnsTheRightContacts()
+//        {
+//            // arrange
+//            var client = factory.CreateClient();
+//            CreateSUT();
+
+//            // act
+//            var response = await client.GetAsync(BaseUrl);
+
+//            // assert
+//            response.EnsureSuccessStatusCode();
+//            var contacts = await response.Content.ReadAsStringAsync();
+//            contacts.Should().Contain("123456789");
+//        }
+
+//        [Fact]
+//        public async void GivenValidContact_WhenCreatedIsCalled_ThenShouldAddToDatabaseTheContact()
+//        {
+//            // Arrange
+//            var client = factory.CreateClient();
+
+//            var user = new User
+//            {
+//                Id = new Guid("fb0c0cbf-cf67-4cc8-babc-63d8b24862b7"),
+//                Type = "f",
+//                FirstName = "John",
+//                LastName = "Doe",
+//                Status = "Active"
+//            };
+
+//            var command = new CreateContactCommand
+//            {
+//                UserId = user.Id,
+//                Phone = "123456789",
+//                Email = "jhondoe@gmail.com"
+//            };
+
+//            // Act
+//            await client.PostAsJsonAsync(BaseUrl, command);
+
+//            // Assert
+//            var contact = await dbContext.Contacts.FirstOrDefaultAsync(b => b.Phone == "123456789");
+//            contact.Should().NotBeNull();
+//        }
+
+//        public void Dispose()
+//        {
+//            GC.SuppressFinalize(this);
+//            dbContext.Database.EnsureDeleted();
+//            dbContext.Dispose();
+//        }
+
+//        private void CreateSUT()
+//        {
+//            var contact = new Contact
+//            {
+//                Id = new Guid("fb0c0cbf-cf67-4cc8-babc-63d8b24862b7"),
+//                UserId = new Guid("fb0c0cbf-cf67-4cc8-babc-63d8b24862b7"),
+//                Phone = "123456789",
+//                Email = "jhondoe@gmail.com"
+//            };
+//            dbContext.Contacts.Add(contact);
+//            dbContext.SaveChanges();
+//        }
+//    }
+//}
+using Application.Use_Cases.Commands.ContactC;
 using Domain.Entities;
 using FluentAssertions;
 using Infrastructure.Persistence;
@@ -15,12 +145,15 @@ namespace SmartRealEstateManagementSystem.IntegrationTests
         private readonly ApplicationDbContext dbContext;
 
         private string BaseUrl = "/api/contacts";
+
         public ContactControllerIntegrationTests(WebApplicationFactory<Program> factory)
         {
+            // Configure factory to use InMemory database
             this.factory = factory.WithWebHostBuilder(builder =>
             {
                 builder.ConfigureServices(services =>
                 {
+                    // Remove existing DbContextOptions<ApplicationDbContext>
                     var descriptor = services.SingleOrDefault(
                         d => d.ServiceType ==
                             typeof(DbContextOptions<ApplicationDbContext>));
@@ -30,6 +163,7 @@ namespace SmartRealEstateManagementSystem.IntegrationTests
                         services.Remove(descriptor);
                     }
 
+                    // Add InMemory database for testing
                     services.AddDbContext<ApplicationDbContext>(options =>
                     {
                         options.UseInMemoryDatabase("InMemoryDbForTesting");
@@ -37,13 +171,14 @@ namespace SmartRealEstateManagementSystem.IntegrationTests
                 });
             });
 
+            // Create service scope to access DbContext
             var scope = this.factory.Services.CreateScope();
             dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             dbContext.Database.EnsureCreated();
         }
 
         [Fact]
-        public async void GivenContacts_WhenGetAllIsCalled_ThenReturnsTheRightContentType()
+        public async Task GivenContacts_WhenGetAllIsCalled_ThenReturnsTheRightContentType()
         {
             // arrange
             var client = factory.CreateClient();
@@ -57,7 +192,7 @@ namespace SmartRealEstateManagementSystem.IntegrationTests
         }
 
         [Fact]
-        public async  void GivenExistingContacts_WhenGetAllIsCalled_ThenReturnsTheRightContacts()
+        public async Task GivenExistingContacts_WhenGetAllIsCalled_ThenReturnsTheRightContacts()
         {
             // arrange
             var client = factory.CreateClient();
@@ -73,7 +208,7 @@ namespace SmartRealEstateManagementSystem.IntegrationTests
         }
 
         [Fact]
-        public async void GivenValidEstate_WhenCreatedIsCalled_Then_ShouldAddToDatabaseTheEstate()
+        public async Task GivenValidContact_WhenCreatedIsCalled_ThenShouldAddToDatabaseTheContact()
         {
             // Arrange
             var client = factory.CreateClient();
@@ -98,16 +233,16 @@ namespace SmartRealEstateManagementSystem.IntegrationTests
             await client.PostAsJsonAsync(BaseUrl, command);
 
             // Assert
-            var contact = dbContext.Contacts.FirstOrDefaultAsync(b => b.Phone == "123456789");
+            var contact = await dbContext.Contacts.FirstOrDefaultAsync(b => b.Phone == "123456789");
             contact.Should().NotBeNull();
         }
+
         public void Dispose()
         {
             GC.SuppressFinalize(this);
             dbContext.Database.EnsureDeleted();
             dbContext.Dispose();
         }
-
 
         private void CreateSUT()
         {
@@ -117,7 +252,6 @@ namespace SmartRealEstateManagementSystem.IntegrationTests
                 UserId = new Guid("fb0c0cbf-cf67-4cc8-babc-63d8b24862b7"),
                 Phone = "123456789",
                 Email = "jhondoe@gmail.com"
-
             };
             dbContext.Contacts.Add(contact);
             dbContext.SaveChanges();
