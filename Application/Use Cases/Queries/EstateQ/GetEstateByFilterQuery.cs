15f686b7411d57c1aed1080d81bb4e4931d62f30
BurcaStefan <burcaiulianstefan@gmail.com>
2024-11-30 18:58:21 +0200

    GetEstatesByFilter

---
+++ Application/Use Cases/Queries/EstateQ/GetEstateByFilterQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTO;
+using MediatR;
+
+namespace Application.Use_Cases.Queries.EstateQ
+{
+    public class GetEstateByFilterQuery : IRequest<List<EstateDto>>
+    {
+        public decimal Price { get; set; }
+        public decimal Size { get; set; }
+        public string? Address { get; set; }
+        public string? Type { get; set; }
+    }
+
+}
