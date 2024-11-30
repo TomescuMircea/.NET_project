840d7cd24c3753d882351cad34ff639bc416e92f
BurcaStefan <burcaiulianstefan@gmail.com>
2024-11-30 19:07:30 +0200

    .

--- Application/Use Cases/QueryHandlers/EstateQH/GetEstateByFilterQueryHandler.cs
+++ Application/Use Cases/QueryHandlers/EstateQH/GetEstateByFilterQueryHandler.cs
@@ -4,11 +4,6 @@ using AutoMapper;
 using Domain.Entities;
 using Domain.Repositories;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Use_Cases.QueryHandlers.EstateQH
 {
