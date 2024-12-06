d10587689d99b160224761caae3b8357d0d9e9c8
Stefan Cotos <stefancotos8@gmail.com>
2024-12-06 20:23:34 +0200

    Register form done

--- smart-real-estate-management-system/src/app/components/register/register.component.ts
+++ smart-real-estate-management-system/src/app/components/register/register.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { PasswordMatchValidator } from './password-match.validator';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit 
+{
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private readonly userService: UserService,
+    private readonly router: Router,) 
+    {
+      this.registerForm = this.fb.group({
+        firstName: ['', [Validators.required, Validators.maxLength(100)]],
+        lastName: ['', [Validators.required, Validators.maxLength(100)]],
+        userName: ['', [Validators.required, Validators.maxLength(100)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+      }, {
+        validator: PasswordMatchValidator('password', 'confirmPassword')
+      });
+    }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+
+      this.userService.register(this.registerForm.value).subscribe(() => {
+        this.router.navigate(['']);
+      });
+    }
+  }
 }
